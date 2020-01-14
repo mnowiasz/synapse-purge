9078df84b80ff26d032a9b6ca60bac1bbaf0b5ee
Mark Nowiasz <mark.nowiasz@neofonie.de>
2020-01-14 14:28:48 +0100

    Testdrive

--- synapsepurge/purge.py
+++ synapsepurge/purge.py
@@ -5,9 +5,14 @@ from synapsepurge import roomlist
 
 def purge():
     my_config = config.Config()
-    my_config.read_config()
-    roomlist.get_rooms(my_config)
-    print(roomlist)
+    error = my_config.read_config()
+    if error:
+        print(error)
+        exit(1)
+
+    rooms = roomlist.get_rooms(my_config)
+    for room in rooms:
+        print(room[0])
 
 if __name__ == '__main__':
     purge()
